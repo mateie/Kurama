71730ab35ff90e05dc5e4b7f502e6a7435c3e9e5
Mateie <mateie@email.com>
2022-05-08 23:00:24 -0700

    added botLogs for the mainguild

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,6 +1,7 @@
 import Event from '@classes/base/Event';
 import Client from '@classes/Client';
 import { IEvent } from '@types';
+import { TextChannel } from 'discord.js';
 
 export default class ReadyEvent extends Event implements IEvent {
     constructor(client: Client) {
@@ -15,6 +16,7 @@ export default class ReadyEvent extends Event implements IEvent {
 
         const mainGuild = await this.client.guilds.fetch('814017098409443339');
         this.client.mainGuild = mainGuild;
+        this.client.botLogs = await mainGuild.channels.fetch('973100098651324446') as TextChannel;
 
         this.client.database.guilds.verify();
         this.client.guilds.cache.forEach(async guild => this.client.database.members.verify(guild));
