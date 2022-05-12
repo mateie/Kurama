36b210482166de4ab8864ef02763c356f3abc20d
Mateie <mateie@email.com>
2022-05-12 02:05:29 -0700

    added complex help command

---
+++ src/commands/info/help.ts
@@ -0,0 +1,58 @@
+import { CommandInteraction } from 'discord.js';
+import Client from '@classes/Client';
+import Command from '@classes/base/Command';
+import { ICommand } from '@types';
+
+export default class HelpCommand extends Command implements ICommand {
+    constructor(client: Client) {
+        super(client);
+
+        this.name = 'help';
+
+        this.data
+            .setName(this.name)
+            .setDescription(this.description)
+            .addSubcommand(subcommand =>
+                subcommand
+                    .setName('category')
+                    .setDescription('View Category for commands')
+                    .addStringOption(option =>
+                        option
+                            .setName('category_view')
+                            .setDescription('Category to view')
+                            .setRequired(true)
+                    )
+            )
+            .addSubcommand(subcommand =>
+                subcommand
+                    .setName('command')
+                    .setDescription('View a Command')
+                    .addStringOption(option =>
+                        option
+                            .setName('command_view')
+                            .setDescription('Command to view')
+                            .setRequired(true)
+                    )
+            );
+    }
+
+    async run(interaction: CommandInteraction) {
+        const { options } = interaction;
+
+        switch (options.getSubcommand()) {
+        case 'category': {
+            const categoryString = options.getString('category_view', true).toLowerCase().trim();
+            const category = this.client.commandHandler.categories.get(categoryString)?.filter(cat => !cat.type);
+            if (!category) return interaction.reply({ content: 'That category does not exist', ephemeral: true });
+            return this.client.util.pagination.helpCategory(interaction, category);
+        }
+        case 'command': {
+            const commandString = options.getString('command_view', true).toLowerCase().trim();
+            const command = this.client.commandHandler.commands.get(commandString);
+            if (!command) return interaction.reply({ content: 'That Command does not exist', ephemeral: true });
+            return this.client.util.pagination.helpCommand(interaction, command.data.toJSON());
+            break;
+        }
+        }
+    }
+}
