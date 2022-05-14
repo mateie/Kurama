35baa01d737f5a0ca725c65fe6f30ce1731d8e62
Mateie <mateie@email.com>
2022-05-14 01:36:51 -0700

    warn commands

---
+++ src/commands/moderation/warn.ts
@@ -0,0 +1,39 @@
+import { CommandInteraction, GuildMember } from 'discord.js';
+import Client from '@classes/Client';
+import Command from '@classes/base/Command';
+import { ICommand } from '@types';
+
+export default class WarnCommand extends Command implements ICommand {
+    constructor(client: Client) {
+        super(client);
+
+        this.name = 'warn';
+        this.description = 'Warn a memebr';
+
+        this.data
+            .setName(this.name)
+            .setDescription(this.description)
+            .addUserOption(option =>
+                option
+                    .setName('member')
+                    .setDescription('Member to warn')
+                    .setRequired(true)
+            )
+            .addStringOption(option =>
+                option
+                    .setName('reason')
+                    .setDescription('REason to warn this member')
+            );
+    }
+
+    async run(interaction: CommandInteraction) {
+        const { options } = interaction;
+
+        const member = options.getMember('member') as GuildMember;
+        const reason = options.getString('reason') || 'No reason specified';
+
+        if (member.user.bot) return interaction.reply({ content: `${member} is a bot`, ephemeral: true });
+        
+        return this.client.moderation.warns.create(interaction, member, reason);
+    }
+}
---
+++ src/commands/moderation/warns.ts
@@ -0,0 +1,44 @@
+import { CommandInteraction, Guild, GuildMember } from 'discord.js';
+import Client from '@classes/Client';
+import Command from '@classes/base/Command';
+import { ICommand } from '@types';
+
+export default class WarnsCommand extends Command implements ICommand {
+    constructor(client: Client) {
+        super(client);
+
+        this.name = 'warns';
+        this.description = 'Check warns of a member';
+
+        this.data
+            .setName(this.name)
+            .setDescription(this.description)
+            .addUserOption(option =>
+                option
+                    .setName('member')
+                    .setDescription('Member to check warns of')
+                    .setRequired(true)
+            );
+    }
+
+    async run(interaction: CommandInteraction) {
+        const { options } = interaction;
+
+        const member = options.getMember('member') as GuildMember;
+
+        if (member.user.bot) return interaction.reply({ content: `${member} is a bot`, ephemeral: true });
+
+        const guild = interaction.guild as Guild;
+
+        const warns = await this.client.moderation.warns.get(member);
+
+        if (warns.length < 1) return interaction.reply({ content: `${member} has no warns`, ephemeral: true });
+
+        const warnMap = warns.map(warn => `
+            **Warned by**: ${guild.members.cache.get(warn.by)}
+            **Reason**: ${warn.reason}
+        `);
+
+        return this.client.util.pagination.default(interaction, warnMap, `${member.user.tag}'s Warns`);
+    }
+}
