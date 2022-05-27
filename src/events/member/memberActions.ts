b6ca400d14617539a434ff1598b4feb8a7610b84
Mateie <mateie@email.com>
2022-05-27 16:52:12 -0700

    added member actions

---
+++ src/events/member/memberActions.ts
@@ -0,0 +1,69 @@
+import Client from '@classes/Client';
+import Event from '@classes/base/Event';
+import { IEvent } from '@types';
+import { ButtonInteraction, Guild, GuildMember, Message } from 'discord.js';
+
+export default class MemberActionsEvent extends Event implements IEvent {
+
+    constructor(client: Client) {
+        super(client);
+
+        this.name = 'interactionCreate';
+    }
+
+    async run(interaction: ButtonInteraction) {
+        if (!interaction.isButton()) return;
+
+        if (![
+            'show_rank',
+            'show_warns',
+            'show_reports',
+            'report_member',
+            'warn_member',
+        ].includes(interaction.customId)) return;
+
+        const guild = interaction.guild as Guild;
+        const message = interaction.message as Message;
+        const member = interaction.member as GuildMember;
+        
+        const target = await guild.members.fetch(message.embeds[0].footer?.text.split(':')[1] as string);
+
+        switch (interaction.customId) {
+        case 'show_rank': {
+            const attachment = await this.client.canvas.member.rank(target);
+            return interaction.reply({ files: [attachment], ephemeral: true});
+        }
+        case 'report_member': {
+            const modal = this.client.util.modal()
+                .setCustomId('report-member-modal')
+                .setTitle(`Reporting ${target.user.tag}`)
+                .setComponents(
+                    this.client.util.modalRow()
+                        .setComponents(this.client.util.input()
+                            .setCustomId('report-member-reason')
+                            .setLabel('Reason')
+                            .setStyle('SHORT')
+                            .setMinLength(4)
+                            .setMaxLength(100)
+                            .setPlaceholder('Type your reason here')
+                            .setRequired(true)    
+                        )
+                );
+
+            return interaction.showModal(modal);
+        }
+        case 'show_warns': {
+            if (!member.permissions.has('VIEW_AUDIT_LOG')) return interaction.reply({ content: 'Not enough permissions', ephemeral: true });
+            const warns = await this.client.moderation.warns.get(target);
+            if (warns.length < 1) return interaction.reply({ content: `${target} has no warns`, ephemeral: true });
+            const mapped = warns.map(warn => `
+                    ***Warned by***: ${guild.members.cache.get(warn.by)}
+                    ***Reason***: ${warn.reason}
+                `);
+
+            return this.client.util.pagination.default(interaction, mapped, `${target.user.tag} Warns`, true);
+        }
+        }
+
+    }
+}
