940fd060543d90fcf23ec2a437d786a30d424603
Mateie <mateie@email.com>
2022-05-29 04:10:14 -0700

    added warn menu

---
+++ src/commands/moderation/warnMenu.ts
@@ -0,0 +1,23 @@
+import { ContextMenuInteraction, Guild } from 'discord.js';
+import Client from '@classes/Client';
+import Menu from '@classes/base/Menu';
+import { IMenu } from '@types';
+
+export default class WarnMenu extends Menu implements IMenu {
+    constructor(client: Client) {
+        super(client);
+
+        this.name = 'Warn Member';
+
+        this.data
+            .setName(this.name)
+            .setType(2);
+    }
+
+    async run(interaction: ContextMenuInteraction) {
+        const guild = interaction.guild as Guild;
+        const member = await guild.members.fetch(interaction.targetId);
+
+        return interaction.showModal(this.client.moderation.warns.modal(member));
+    }
+}
