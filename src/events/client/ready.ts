69afe6c3b1a1a00e28148a9d644416f4e1dcb6a3
Mateie <mateie@email.com>
2022-06-08 01:20:02 -0700

    more stuff

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -14,8 +14,12 @@ export default class ReadyEvent extends Event implements IEvent {
     async run() {
         console.log(`Ready! Logged in as ${this.client.user?.tag}`);
 
-        this.client.mainGuild = await this.client.guilds.fetch("814017098409443339");;
-        this.client.botLogs = await this.client.mainGuild.channels.fetch("974256729661509632") as TextChannel;
+        this.client.mainGuild = await this.client.guilds.fetch(
+            "814017098409443339"
+        );
+        this.client.botLogs = (await this.client.mainGuild.channels.fetch(
+            "974256729661509632"
+        )) as TextChannel;
 
         this.client.database.guilds.verify();
         this.client.database.users.verify();
