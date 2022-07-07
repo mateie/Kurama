776a566921e24ee298c962a9326bd400abc40bbd
Mateie <mateie@email.com>
2022-07-07 02:19:15 -0700

    removed useless line of code

--- src/events/guild/guildJoin.ts
+++ src/events/guild/guildJoin.ts
@@ -12,6 +12,5 @@ export default class GuildJoinEvent extends Event implements IEvent {
 
     async run(guild: Guild) {
         this.client.database.guilds.verify(guild);
-        this.client.deploy();
     }
 }
