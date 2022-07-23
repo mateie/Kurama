301cc732ebb7016e3fcc4177c5fda9a97ba96820
Mateie <mateie@email.com>
2022-07-23 12:23:32 -0700

    moved pagination to their classes

--- src/classes/games/shinobi/clans/index.ts
+++ src/classes/games/shinobi/clans/index.ts
@@ -1,16 +1,19 @@
+import Client from "@classes/Client";
 import ShinobiGame from "..";
-import { Collection } from "discord.js";
+import { Collection, CommandInteraction, Message } from "discord.js";
 import { ShinobiClan } from "@types";
 
 import Clans from "./Clans";
 import Shinobi from "@schemas/Shinobi";
 
 export default class ShinobiClans {
+    private readonly client: Client;
     private readonly game: ShinobiGame;
 
     private readonly list: Collection<string, ShinobiClan>;
 
     constructor(game: ShinobiGame) {
+        this.client = game.client;
         this.game = game;
 
         this.list = new Collection();
@@ -19,7 +22,7 @@ export default class ShinobiClans {
 
     get(name: string) {
         const clan = this.list.get(name);
-        if (!clan) return false;
+        if (!clan) return null;
         return clan;
     }
 
@@ -27,8 +30,8 @@ export default class ShinobiClans {
 
     random = () => this.list.random();
 
-    embed(clan: ShinobiClan) {
-        return this.game.client.util
+    embed = (clan: ShinobiClan) =>
+        this.client.util
             .embed()
             .setTitle(clan.name)
             .setDescription(
@@ -44,12 +47,72 @@ export default class ShinobiClans {
             )
             .setThumbnail(clan.icon)
             .setFooter({ text: `Members: ${clan.members}` });
+
+    async pagination(interaction: CommandInteraction) {
+        const clans = this.getAll();
+
+        let page = 0;
+
+        const buttons = [
+            this.client.util
+                .button()
+                .setCustomId("previous_page")
+                .setLabel("⬅️")
+                .setStyle("SECONDARY"),
+            this.client.util
+                .button()
+                .setCustomId("next_page")
+                .setLabel("➡️")
+                .setStyle("SECONDARY")
+        ];
+
+        const row = this.client.util.row().addComponents(buttons);
+
+        const embeds = clans.map((clan) => this.embed(clan));
+
+        if (interaction.deferred === false) await interaction.deferReply();
+
+        const message = (await interaction.editReply({
+            embeds: [embeds[page]],
+            components: [row]
+        })) as Message;
+
+        const collector = message.createMessageComponentCollector({
+            filter: (i) =>
+                i.customId === buttons[0].customId ||
+                i.customId === buttons[1].customId,
+            time: 60000
+        });
+
+        collector
+            .on("collect", async (i) => {
+                switch (i.customId) {
+                    case buttons[0].customId:
+                        page = page > 0 ? --page : embeds.length - 1;
+                        break;
+                    case buttons[1].customId:
+                        page = page + 1 < embeds.length ? ++page : 0;
+                        break;
+                    default:
+                        break;
+                }
+
+                await i.deferUpdate();
+                await i.editReply({
+                    embeds: [embeds[page]],
+                    components: [row]
+                });
+
+                collector.resetTimer();
+            })
+            .on("end", () => {
+                message.delete();
+            });
     }
 
-    private setup() {
+    private setup = () =>
         Clans.forEach(async (clan) => {
             clan.members = (await Shinobi.find({ clan: clan.id })).length;
             this.list.set(clan.id, clan);
         });
-    }
 }
--- src/classes/games/shinobi/villages/index.ts
+++ src/classes/games/shinobi/villages/index.ts
@@ -1,15 +1,18 @@
 import ShinobiGame from "..";
-import { Collection } from "discord.js";
+import { Collection, CommandInteraction, Message } from "discord.js";
 import { ShinobiVillage } from "@types";
 import Villages from "./Villages";
 import Shinobi from "@schemas/Shinobi";
+import Client from "@classes/Client";
 
 export default class ShinobiVillages {
+    private readonly client: Client;
     private readonly game: ShinobiGame;
 
     private readonly list: Collection<string, ShinobiVillage>;
 
     constructor(game: ShinobiGame) {
+        this.client = game.client;
         this.game = game;
 
         this.list = new Collection();
@@ -18,7 +21,7 @@ export default class ShinobiVillages {
 
     get(name: string) {
         const clan = this.list.get(name);
-        if (!clan) return false;
+        if (!clan) return null;
         return clan;
     }
 
@@ -26,21 +29,81 @@ export default class ShinobiVillages {
 
     random = () => this.list.random();
 
-    embed(village: ShinobiVillage) {
-        return this.game.client.util
+    embed = (village: ShinobiVillage) =>
+        this.client.util
             .embed()
             .setTitle(`${village.name.en} (${village.name.jp})`)
             .setDescription(village.description)
             .setThumbnail(village.icon)
             .setFooter({ text: `Population: ${village.population}` });
+
+    async pagination(interaction: CommandInteraction) {
+        const villages = this.getAll();
+
+        let page = 0;
+
+        const buttons = [
+            this.client.util
+                .button()
+                .setCustomId("previous_page")
+                .setLabel("⬅️")
+                .setStyle("SECONDARY"),
+            this.client.util
+                .button()
+                .setCustomId("next_page")
+                .setLabel("➡️")
+                .setStyle("SECONDARY")
+        ];
+
+        const row = this.client.util.row().addComponents(buttons);
+
+        const embeds = villages.map((village) => this.embed(village));
+
+        if (interaction.deferred === false) await interaction.deferReply();
+
+        const message = (await interaction.editReply({
+            embeds: [embeds[page]],
+            components: [row]
+        })) as Message;
+
+        const collector = message.createMessageComponentCollector({
+            filter: (i) =>
+                i.customId === buttons[0].customId ||
+                i.customId === buttons[1].customId,
+            time: 60000
+        });
+
+        collector
+            .on("collect", async (i) => {
+                switch (i.customId) {
+                    case buttons[0].customId:
+                        page = page > 0 ? --page : embeds.length - 1;
+                        break;
+                    case buttons[1].customId:
+                        page = page + 1 < embeds.length ? ++page : 0;
+                        break;
+                    default:
+                        break;
+                }
+
+                await i.deferUpdate();
+                await i.editReply({
+                    embeds: [embeds[page]],
+                    components: [row]
+                });
+
+                collector.resetTimer();
+            })
+            .on("end", () => {
+                message.delete();
+            });
     }
 
-    private setup() {
+    private setup = () =>
         Villages.forEach(async (village) => {
             village.population = (
                 await Shinobi.find({ village: village.id })
             ).length;
             this.list.set(village.id, village);
         });
-    }
 }
