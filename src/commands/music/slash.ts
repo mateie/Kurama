c81715d18205ea1030034b8735fbeb9adf55daf6
Mateie <mateie@email.com>
2022-07-22 17:39:29 -0700

    formatting

--- src/commands/music/slash.ts
+++ src/commands/music/slash.ts
@@ -3,7 +3,7 @@ import {
     Guild,
     GuildMember,
     TextChannel,
-    VoiceChannel,
+    VoiceChannel
 } from "discord.js";
 import Client from "@classes/Client";
 import Command from "@classes/base/Command";
@@ -99,7 +99,7 @@ export default class MusicCommand extends Command implements ICommand {
             return interaction.reply({
                 content:
                     "You must be in a voice channel to be able to use the music commands",
-                ephemeral: true,
+                ephemeral: true
             });
 
         if (
@@ -108,13 +108,13 @@ export default class MusicCommand extends Command implements ICommand {
         )
             return interaction.reply({
                 content: `I'm already playing music in ${guild.me.voice.channel}`,
-                ephemeral: true,
+                ephemeral: true
             });
 
         if (member.voice.deaf)
             return interaction.reply({
                 content: "You cannot play music when deafened",
-                ephemeral: true,
+                ephemeral: true
             });
 
         let queue = this.client.music.getQueue(guild);
@@ -123,7 +123,7 @@ export default class MusicCommand extends Command implements ICommand {
             case "play": {
                 if (!queue) {
                     queue = this.client.music.createQueue(guild, {
-                        metadata: channel,
+                        metadata: channel
                     });
 
                     try {
@@ -133,23 +133,23 @@ export default class MusicCommand extends Command implements ICommand {
                         queue.destroy();
                         return await interaction.reply({
                             content: "Could not join your voice channel",
-                            ephemeral: true,
+                            ephemeral: true
                         });
                     }
                 }
 
                 await interaction.deferReply({
-                    ephemeral: true,
+                    ephemeral: true
                 });
 
                 const query = options.getString("query") as string;
                 const result = await this.client.music.search(query, {
-                    requestedBy: interaction.user,
+                    requestedBy: interaction.user
                 });
 
                 if (!result.tracks[0]) {
                     await interaction.followUp({
-                        content: `Track **${query} was not found`,
+                        content: `Track **${query} was not found`
                     });
                     break;
                 }
@@ -161,7 +161,7 @@ export default class MusicCommand extends Command implements ICommand {
 
                 await interaction.followUp({
                     content: "Track/Playlist Recieved",
-                    ephemeral: true,
+                    ephemeral: true
                 });
 
                 break;
@@ -170,38 +170,38 @@ export default class MusicCommand extends Command implements ICommand {
                 if (!queue)
                     return interaction.reply({
                         content: "Music is not playing",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 if (queue.connection.status === "paused")
                     return interaction.reply({
                         content: "Music is already paused",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 queue.setPaused(true);
                 return interaction.reply({
                     content: "Track paused",
-                    ephemeral: true,
+                    ephemeral: true
                 });
             }
             case "resume": {
                 if (!queue)
                     return interaction.reply({
                         content: "Music is not playing",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 if (queue.connection.status === "playing")
                     return interaction.reply({
                         content: "Music is already playing",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 queue.setPaused(false);
                 return interaction.reply({
                     content: "Track resumed",
-                    ephemeral: true,
+                    ephemeral: true
                 });
             }
             case "stop": {
@@ -209,26 +209,26 @@ export default class MusicCommand extends Command implements ICommand {
                     return interaction.reply({
                         content:
                             "You cannot stop the player, not enough permissions",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 if (!queue)
                     return interaction.reply({
                         content: "Music is not playing",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 queue.stop();
                 return interaction.reply({
-                    content: "Player stopped",
-                    ephemeral: true,
+                    content: "Music has been stopped",
+                    ephemeral: true
                 });
             }
             case "queue": {
                 if (!queue)
                     return interaction.reply({
                         content: "Music is not playing",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 const mapped = queue.tracks.map((track, index) => {
@@ -242,7 +242,7 @@ export default class MusicCommand extends Command implements ICommand {
                 if (chunked.length < 1)
                     return await interaction.reply({
                         content: "There are no upcoming tracks",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 this.util.pagination.default(
@@ -256,12 +256,12 @@ export default class MusicCommand extends Command implements ICommand {
                 if (!queue)
                     return interaction.reply({
                         content: "Music is not playing",
-                        ephemeral: true,
+                        ephemeral: true
                     });
                 if (queue.tracks.length < 1) {
                     return interaction.reply({
                         content: "There are no upcoming tracks to skip to",
-                        ephemeral: true,
+                        ephemeral: true
                     });
                 }
 
@@ -272,14 +272,14 @@ export default class MusicCommand extends Command implements ICommand {
                 if (currentTrack.requestedBy.id !== member.id)
                     return interaction.reply({
                         content: `You didn't request this track, ask ${requestedBy} to skip the track, because they requested it`,
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 const position = options.getNumber("to");
                 if (!position) {
                     queue.skip();
                     return interaction.reply({
-                        content: "Current track skipped",
+                        content: "Current track skipped"
                     });
                 }
                 const skipTo = position - 1;
@@ -287,52 +287,52 @@ export default class MusicCommand extends Command implements ICommand {
                 queue.skipTo(skipTo);
                 return interaction.reply({
                     content: `Skipped to: **${track.author} - ${track.title}**`,
-                    ephemeral: true,
+                    ephemeral: true
                 });
             }
             case "shuffle": {
                 if (!queue)
                     return interaction.reply({
                         content: "Music is not playing",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 if (queue.tracks.length < 2)
                     return await interaction.reply({
                         content: "There are no upcoming tracks",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 return interaction.reply({
                     content: "Queue shuffled",
-                    ephemeral: true,
+                    ephemeral: true
                 });
             }
             case "seek": {
                 if (!queue)
                     return interaction.reply({
                         content: "Music is not playing",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 const duration = options.getString("duration") as string;
                 const durationPattern = /^[0-5]?[0-9](:[0-5][0-9]){1,2}$/;
                 if (!durationPattern.test(duration))
                     return interaction.reply({
-                        content: "Duration provided in incorrect format",
+                        content: "Duration provided in incorrect format"
                     });
 
                 const durationMs = this.util.durationMs(duration);
                 if (durationMs > queue.current.durationMS)
                     return interaction.reply({
                         content: "Duration you provided exceeds track duration",
-                        ephemeral: true,
+                        ephemeral: true
                     });
 
                 queue.seek(durationMs);
                 return interaction.reply({
                     content: `Seeked to ${duration}`,
-                    ephemeral: true,
+                    ephemeral: true
                 });
             }
         }
